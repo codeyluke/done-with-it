bcedd8efc9e70979c53b8881aff20ed934238694
codeyluke <lukekennedy212@gmail.com>
2021-07-24 22:48:24 +0800

    update deprecated code

--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableWithoutFeedback,
-  Alert,
-} from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Image, TouchableWithoutFeedback, Alert } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
-import colors from "../config/colors";
+import colors from '../config/colors';
 
 function ImageInput({ imageUri, onChangeImage }) {
   useEffect(() => {
@@ -17,16 +11,16 @@ function ImageInput({ imageUri, onChangeImage }) {
   }, []);
 
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access the library.");
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library.');
   };
 
   const handlePress = () => {
     if (!imageUri) selectImage();
     else
-      Alert.alert("Delete", "Are you sure you want to delete this image?", [
-        { text: "Yes", onPress: () => onChangeImage(null) },
-        { text: "No" },
+      Alert.alert('Delete', 'Are you sure you want to delete this image?', [
+        { text: 'Yes', onPress: () => onChangeImage(null) },
+        { text: 'No' },
       ]);
   };
 
@@ -38,20 +32,14 @@ function ImageInput({ imageUri, onChangeImage }) {
       });
       if (!result.cancelled) onChangeImage(result.uri);
     } catch (error) {
-      console.log("Error reading an image", error);
+      console.log('Error reading an image', error);
     }
   };
 
   return (
     <TouchableWithoutFeedback onPress={handlePress}>
       <View style={styles.container}>
-        {!imageUri && (
-          <MaterialCommunityIcons
-            color={colors.medium}
-            name="camera"
-            size={40}
-          />
-        )}
+        {!imageUri && <MaterialCommunityIcons color={colors.medium} name="camera" size={40} />}
         {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
       </View>
     </TouchableWithoutFeedback>
@@ -60,18 +48,18 @@ function ImageInput({ imageUri, onChangeImage }) {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
+    alignItems: 'center',
     backgroundColor: colors.light,
     borderRadius: 15,
     height: 100,
-    justifyContent: "center",
+    justifyContent: 'center',
     marginVertical: 10,
-    overflow: "hidden",
+    overflow: 'hidden',
     width: 100,
   },
   image: {
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
 });
 
--- app/hooks/useLocation.js
+++ app/hooks/useLocation.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react";
-import * as Location from "expo-location";
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 
 export default useLocation = () => {
   const [location, setLocation] = useState();
 
   const getLocation = async () => {
     try {
-      const { granted } = await Location.requestPermissionsAsync();
+      const { granted } = await Location.requestForegroundPermissionsAsync();
       if (!granted) return;
       const {
         coords: { latitude, longitude },
