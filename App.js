fa6774acb024c7ab1414714a980fbf5bda4728be
codeyluke <lukekennedy212@gmail.com>
2021-07-24 21:10:29 +0800

    section 1

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import AppPicker from './app/components/Picker';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <>
+      <ListingEditScreen />
+    </>
+  );
 }
